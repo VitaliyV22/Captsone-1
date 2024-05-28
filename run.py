e18e9df0478d43ebde67d2b3bf54cf2726e2b477
VitaliyV22 <vitanskyyv@gmail.com>
2024-05-27 23:25:21 -0500

    .

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ app = create_app()
 migrate = Migrate(app, db)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000)) 
+    port = int(os.environ.get('PORT', 10000)) 
     
     app.run(debug=True, host='0.0.0.0', port=port)
