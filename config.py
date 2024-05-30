1db4ad38e4b15f5097e14da16bde6aef42004ab1
VitaliyV22 <vitanskyyv@gmail.com>
2024-05-29 23:07:01 -0500

    .

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
-
+import psycopg2
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://vlad:2207@localhost/finance'
+    
+    DATABASE_URL = os.environ['DATABASE_URL']
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
