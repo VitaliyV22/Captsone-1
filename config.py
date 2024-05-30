34fba006d32aa8d382e4419c44f6e49fb6a8d69c
VitaliyV22 <vitanskyyv@gmail.com>
2024-05-29 22:27:21 -0500

    .

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgres://uateiulouief8n:p69fcff319c744512f17c8401808374a4ea54f1d1260a64be8f771b0af85bdfaa@ceqbglof0h8enj.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/deltl1lsbe6b4q'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
